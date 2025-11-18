c82aa278de3d29c7977f6bed1dcaae638a43f600
Gaelle Ozon <gozon@student.42.fr>
2025-11-18 13:55:55 +0100

    Start strcpy

--- srcs/ft_strcpy.s
+++ srcs/ft_strcpy.s
@@ -16,3 +16,11 @@ ft_strcpy:
     mov     rbp, rsp        ; define the new stack base
     push    rdi             ; save rdi (first argument) so I can increment it
     push    rsi             ; save rsi (second argument) so I can increment it
+
+
+    cmp     [rdi], 0        ; compare the value pointed at by rdi (first char) with 0
+    jz      end_while       ; jump to end_while if zflag = 1 (ie cmp returned 0)
+
+while:
+    movsb                   ; move the byte in rdi to rsi + increment
+    cmp     [rdi], 0        ; compare the value pointed at by rdi with 0
