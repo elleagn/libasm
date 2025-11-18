b397744281c00d44c87e4c53480cf9cea12252df
Gaelle Ozon <gozon@student.42.fr>
2025-11-18 08:25:49 +0100

    Improve tests and give up use of assert

--- main.c
+++ main.c
@@ -1,19 +1,35 @@
 #include "libasm.h"
+#include "string.h"
 
 void test_strlen() {
     size_t res;
-    printf("Test ft_strlen(\"blbl\") == 4 :");
+    printf("Test ft_strlen(\"blbl\") == 4: ");
     res = ft_strlen("blbl");
     if (res == 4)
         printf(GREEN"OK!\n"RESET);
     else
         printf(RED"error (%lu)\n"RESET, res);
- assert(ft_strlen("") == 0);
- printf("Test ft_strlen(\"\") == 0 : "GREEN"OK!\n"RESET);
- assert(ft_strlen("b") == 1);
- printf("Test ft_strlen(\"b\") == 1 : "GREEN"OK!\n"RESET);
- assert(ft_strlen("blblblblblblblblblbl") == 20);
- printf("Test ft_strlen(\"blblblblblblblblblbl\") == 20 : "GREEN"OK!\n"RESET);
+
+    printf("Test ft_strlen(\"\") == 0: ");
+    res = ft_strlen("");
+    if (res == 0)
+        printf(GREEN"OK!\n"RESET);
+    else
+        printf(RED"error (%lu)\n"RESET, res);
+
+    printf("Test ft_strlen(\"b\") == 1: ");
+    res = ft_strlen("b");
+    if (res == 1)
+        printf(GREEN"OK!\n"RESET);
+    else
+        printf(RED"error (%lu)\n"RESET, res);
+
+    printf("Test ft_strlen(\"blblblblblblblblblbl\") == 20: ");
+    res = ft_strlen("blblblblblblblblblbl");
+    if (res == 20)
+        printf(GREEN"OK!\n"RESET);
+    else
+        printf(RED"error (%lu)\n"RESET, res);
 }
 
 int main(void) {
