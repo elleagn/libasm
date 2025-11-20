89b5daad158ec7ea8dd10d36f0d33afdeed41d56
Gaelle Ozon <gozon@student.42.fr>
2025-11-20 08:58:57 +0100

    Fix strcmp

--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -17,20 +17,19 @@ ft_strcmp:
     xor eax, eax
 
 while:
+    cmpsb                       ; compare the bytes + increment
+    jnz     different           ; result wan't null: strings are different
+    cmp     byte [rdi - 1], al  ; check if at the end of s1
+    jnz     while               ; continue if there are still bytes to compare
 
-    and al, byte [rsi]    ; check for the end of the string
-    jz  end_string       ; go to end if one is zero
-    mov al, byte [rdi]
-    cmpsb           ; compare the bytes + increment
-    jz  while           ; continue if bytes are equal (zf == 1)
+    ret                         ; we reached the end of s1: strings are equal
 
-    mov al, byte [rsi - 1]
-    sub al, byte [rdi - 1]
+different:
+    js     positive             ; if rsi < rdi (ie s1 > s2)
+    mov  eax, -1                ; strcmp returns 1 / -1 not the difference between strings
     ret
 
-end_string:
-
-    movsx rax, byte [rdi]
-    movsx rcx, byte [rsi]
-    sub rax, rcx
+positive:
+    mov eax, 1
     ret
+
