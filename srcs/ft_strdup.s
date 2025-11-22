c35115c647412c19efae3746b7d643cecc01760d
gozon <gozon@student.42.fr>
2025-11-22 07:43:40 +0100

    Fix strdup

--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -17,30 +17,32 @@ extern  ft_strcpy
 ft_strdup:
 
 ;   Create the stack frame
-    push        rbp
-    mov         rbp, rsp
-    sub         rsp, 16
+
+    push        rbp                 ; save the stack (protected register)
+    mov         rbp, rsp            ; new base pointer
+    sub         rsp, 16             ; allocates space for local variables
 
 ;   Getting the string length for malloc
-    mov         [rsp + 8], rdi  ; save the string address
-    call        ft_strlen       ; get the string length
-    mov         rax, rdi        ; give the string size as argument for malloc
-    inc         rax             ; add space for the terminating null byte
+
+    mov         [rsp + 8], rdi      ; save the string address
+    call        ft_strlen           ; get the string length
+    mov         rdi, rax            ; copy the string size in rdi for malloc's first arg
+    inc         rax                 ; add space for the terminating null byte
 
 ;   Allocating space with malloc
-    call        malloc
-    xor         rcx, rcx
-    cmp         rax, rcx
-    jz          prolog
-    mov         [rsp], rax
 
-;   Copying the string into the allocated memory with ft_strcmpy
-    mov         rdi, rax
-    mov         rsi, [rsp + 8]
-    call        ft_strcpy
-    mov         rax, [rsp]
+    call        malloc wrt ..plt    ; calling malloc with option for pie
+    xor         rcx, rcx            ; zero rcx for comparison
+    cmp         rax, rcx            ; check for error (ie malloc returns null)
+    jz          prolog              ; finish function if error
+
+;   Copying the string into the allocated memory with ft_strcpy
+
+    mov         rdi, rax            ; move the allocated memory address in rdi
+    mov         rsi, [rsp + 8]      ; get the source string address from stack
+    call        ft_strcpy           ; copy the string into the allocated memory
 
 prolog:
-    add         rsp, 16                     ; clear the stack
-    pop         rbp                         ; return base to its initial address
-    ret                                     ; finish function
+    add         rsp, 16             ; clear the stack
+    pop         rbp                 ; return base to its initial address
+    ret                             ; finish function
