224305421621ca6b3605fe98b0b9820ae4a24cba
gozon <gozon@student.42.fr>
2025-11-23 21:27:31 +0100

    Start to separate srtfind

---
+++ srcs/ft_strfind_bonus.s
@@ -0,0 +1,7 @@
+;   Program: ft_strfind
+;
+;   Finds a character in a string
+;
+;   Input:  str - the string to search
+;           c - the char to search for
+;           len - the length of the string
